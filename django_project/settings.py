a63ca45c75995ba56c19ae88e3b0a74f32230dda
Anthony Candaele <info@anthonycandaele.be>
2026-05-18 14:57:09 +0200

    newspaper app

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from tkinter import E
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,8 +40,10 @@ INSTALLED_APPS = [
     # 3rd party apps
     "crispy_forms",
     "crispy_bootstrap5",
+    # local apps
     "accounts",
     "pages",
+    "articles",
 ]
 
 MIDDLEWARE = [
